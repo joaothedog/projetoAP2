737ffe1acc46cdf90fb68fb61ab20d92dbf38cf5
joaothedog <76020152+joaothedog@users.noreply.github.com>
2020-12-18 20:24:24 -0300

    NEW CONTROLLER 1.2

--- src/ConcessionariaPop/controller/FXMLCadastroCarroViewController.java
+++ src/ConcessionariaPop/controller/FXMLCadastroCarroViewController.java
@@ -1,12 +1,9 @@
 package ConcessionariaPop.controller;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
@@ -18,60 +15,13 @@ public class FXMLCadastroCarroViewController implements Initializable {
     private AnchorPane anchorPaneMain;
     @FXML
     private AnchorPane anchorPaneHome;
-  
-    @FXML
-    public void handleCloseWindow(MouseEvent event) {
-        System.exit(0);
-    }
-    public void mostraHome(MouseEvent event) throws IOException {
-        handleLoadHome();
-    }
-    
-    private void handleLoadHome() throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("/ConcessionariaPop/view/HomeView.fxml"));
-        anchorpane.getChildren().setAll(root);
-    }
-    
-    public void mostraCadastro(MouseEvent event) throws IOException {
-        handleLoadCadastro();
-    }
-    
-    private void handleLoadCadastro() throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("/ConcessionariaPop/view/CadastroView.fxml"));
-        anchorpane.getChildren().setAll(root);
-    }
-    
-    public void mostraSobreNos(MouseEvent event) throws IOException {
-        handleLoadSobreNos();
-    }
-    
-    private void handleLoadSobreNos() throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("/ConcessionariaPop/view/SobreNosView.fxml"));
-        anchorpane.getChildren().setAll(root);
-    }
-    
-    public void mostraCadastroCarro(MouseEvent event) throws IOException {
-        handleLoadCadastroCarro();
-    }
-    
-    private void handleLoadCadastroCarro() throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("/ConcessionariaPop/view/CadastroCarroView.fxml"));
-        anchorpane.getChildren().setAll(root);
-    }
-    public void mostraAgendamento(MouseEvent event) throws IOException {
-        handleLoadAgendamento();
-    }
-    
-    private void handleLoadAgendamento() throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("/ConcessionariaPop/view/AgendamentoView.fxml"));
-        anchorpane.getChildren().setAll(root);
-    }
-    
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
 
-    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    @FXML
+    private void handleCloseWindow(MouseEvent event) {
+    }
+   
 }
